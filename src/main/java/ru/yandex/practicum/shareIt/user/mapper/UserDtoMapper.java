eed2999ebccc459ed85da8d9f38bb88efb1e12e1
vadimkatkov <vmalyh777@gmail.com>
2025-01-07 00:58:23 +0300

    Добавить проверку заполнение недостающих полей

--- src/main/java/ru/yandex/practicum/shareIt/user/mapper/UserDtoMapper.java
+++ src/main/java/ru/yandex/practicum/shareIt/user/mapper/UserDtoMapper.java
@@ -6,7 +6,9 @@ import ru.yandex.practicum.shareIt.user.dto.UserDto;
 public class UserDtoMapper {
     public static UserDto map(User user) {
         return UserDto.builder()
+                .id(user.getId())
                 .name(user.getName())
+                .email(user.getEmail())
                 .build();
     }
 }
