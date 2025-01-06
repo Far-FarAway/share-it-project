85e90c7b5fa56cba9cea5ab54b530277a7810c04
vadimkatkov <vmalyh777@gmail.com>
2025-01-07 01:02:09 +0300

    Добавить проверку списка отзывов на null

--- src/main/java/ru/yandex/practicum/shareIt/item/mapper/ItemDtoMapper.java
+++ src/main/java/ru/yandex/practicum/shareIt/item/mapper/ItemDtoMapper.java
@@ -2,20 +2,26 @@ package ru.yandex.practicum.shareIt.item.mapper;
 
 import ru.yandex.practicum.shareIt.item.Item;
 import ru.yandex.practicum.shareIt.item.dto.ItemDto;
+import ru.yandex.practicum.shareIt.review.Review;
 import ru.yandex.practicum.shareIt.review.dto.ReviewDto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ItemDtoMapper {
     public static ItemDto map(Item item) {
-        List<ReviewDto> reviewDtoList =  item.getReviews().stream()
-                .map(review -> {
-                    return ReviewDto.builder()
-                            .reviewerName(review.getReviewerName())
-                            .description(review.getDescription())
-                            .build();
-                })
-                .toList();
+        List<Review> reviewList =  item.getReviews();
+        List<ReviewDto> reviewDtoList = new ArrayList<>();
+        if (reviewList != null) {
+            reviewDtoList = reviewList.stream()
+                    .map(review -> {
+                        return ReviewDto.builder()
+                                .reviewerName(review.getReviewerName())
+                                .description(review.getDescription())
+                                .build();
+                    })
+                    .toList();
+        }
 
         return ItemDto.builder()
                 .name(item.getName())
--- src/main/java/ru/yandex/practicum/shareIt/item/mapper/ItemMapper.java
+++ src/main/java/ru/yandex/practicum/shareIt/item/mapper/ItemMapper.java
@@ -3,19 +3,25 @@ package ru.yandex.practicum.shareIt.item.mapper;
 import ru.yandex.practicum.shareIt.item.Item;
 import ru.yandex.practicum.shareIt.item.dto.ItemDto;
 import ru.yandex.practicum.shareIt.review.Review;
+import ru.yandex.practicum.shareIt.review.dto.ReviewDto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ItemMapper {
     public static Item map(long userId, ItemDto itemDto) {
-        List<Review> reviewList = itemDto.getReviews().stream()
-                .map(reviewDto -> {
-                    return Review.builder()
-                            .reviewerName(reviewDto.getReviewerName())
-                            .description(reviewDto.getDescription())
-                            .build();
-                })
-                .toList();
+        List<ReviewDto> reviewDtoList = itemDto.getReviews();
+        List<Review> reviewList = new ArrayList<>();
+        if (reviewDtoList != null) {
+            reviewList = reviewDtoList.stream()
+                    .map(reviewDto -> {
+                        return Review.builder()
+                                .reviewerName(reviewDto.getReviewerName())
+                                .description(reviewDto.getDescription())
+                                .build();
+                    })
+                    .toList();
+        }
 
         return Item.builder()
                 .id(userId)
